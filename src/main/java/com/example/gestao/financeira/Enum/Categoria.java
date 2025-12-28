74ff0a308346c78e34b5a92bf206dcca9d4a9c58
caua <cauacouto022@gmail.com>
2025-12-28 20:26:03 -0300

    feat: enums tipo e Categoria.java

---
+++ src/main/java/com/example/gestao/financeira/Enum/Categoria.java
@@ -0,0 +1,12 @@
+package com.example.gestao.financeira.Enum;
+
+public enum Categoria {
+
+    ALIMENTACAO,
+    TRANSPORTE,
+    MORADIA,
+    LAZER,
+    SAUDE,
+    EDUCACAO,
+    OUTROS
+}
---
+++ src/main/java/com/example/gestao/financeira/Enum/Tipo.java
@@ -0,0 +1,7 @@
+package com.example.gestao.financeira.Enum;
+
+public enum Tipo {
+
+    RECEITA,
+    DESPESA
+}
