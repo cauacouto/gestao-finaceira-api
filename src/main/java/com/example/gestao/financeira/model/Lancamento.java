2eb1174fc6f5fac1458a30fa28b7648bd97d29d8
caua <cauacouto022@gmail.com>
2026-01-16 00:46:33 -0300

    update: prepesit. costrutor e anotação jsonformatter

--- src/main/java/com/example/gestao/financeira/model/Lancamento.java
+++ src/main/java/com/example/gestao/financeira/model/Lancamento.java
@@ -3,20 +3,24 @@ package com.example.gestao.financeira.model;
 import com.example.gestao.financeira.Enum.Categoria;
 import com.example.gestao.financeira.Enum.Tipo;
 import com.example.gestao.financeira.Enum.Moeda;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.text.DateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Table(name = "lancamento")
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
 public class Lancamento {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,6 +37,7 @@ public class Lancamento {
     @Enumerated(EnumType.STRING)
     private Moeda moeda;
 
+    @Column(nullable = false,updatable = false)
     private LocalDate data;
 
     @Enumerated(EnumType.STRING)
@@ -41,5 +46,17 @@ public class Lancamento {
     @Enumerated(EnumType.STRING)
     private Tipo tipo;
 
+    @PrePersist
+    public void PrePersist(){
+      this.data = LocalDate.now();
+    }
 
+    public Lancamento(String descricao, BigDecimal valor, Moeda moeda, Tipo tipo, Categoria categoria) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.moeda = moeda;
+        this.tipo = tipo;
+        this.categoria = categoria;
+
+    }
 }
