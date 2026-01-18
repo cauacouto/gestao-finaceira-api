d2f18e51285b8d82674796538982962d5d92663e
caua <cauacouto022@gmail.com>
2026-01-18 02:09:31 -0300

    feat:implementa lancamentoRepository

--- src/main/java/com/example/gestao/financeira/Repository/LacamentoRepository.java
+++ src/main/java/com/example/gestao/financeira/Repository/LacamentoRepository.java
@@ -2,6 +2,13 @@ package com.example.gestao.financeira.Repository;
 
 import com.example.gestao.financeira.model.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LacamentoRepository extends JpaRepository<Lancamento,Integer> {
+
+    @Query("SELECT l FROM Lancamento l WHERE MONTH(l.data) = :mes AND YEAR(l.data) = :ano")
+    List<Lancamento> findByMesEAno(@Param("mes") int mes, @Param("ano") int ano);
 }
