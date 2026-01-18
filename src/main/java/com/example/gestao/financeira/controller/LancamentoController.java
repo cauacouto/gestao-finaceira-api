30fb03bd483eb035d578066cebe0083c5047bac7
caua <cauacouto022@gmail.com>
2026-01-18 02:08:48 -0300

    feat:implementa gerarRelatorio get e atualizar

--- src/main/java/com/example/gestao/financeira/controller/LancamentoController.java
+++ src/main/java/com/example/gestao/financeira/controller/LancamentoController.java
@@ -1,14 +1,12 @@
 package com.example.gestao.financeira.controller;
 
-import com.example.gestao.financeira.DTO.LacamentoDto;
+import com.example.gestao.financeira.DTO.LacamentoRequestDto;
 import com.example.gestao.financeira.DTO.LacamentoResponseDto;
+import com.example.gestao.financeira.DTO.RelatorioResumoDto;
 import com.example.gestao.financeira.service.LacamentoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -20,10 +18,15 @@ public class LancamentoController {
         this.lacamentoService = lacamentoService;
     }
 @PostMapping
-    public ResponseEntity<LacamentoResponseDto> criarLancamento(@RequestBody LacamentoDto dto) {
+    public ResponseEntity<LacamentoResponseDto> criarLancamento(@RequestBody LacamentoRequestDto dto) {
          LacamentoResponseDto response =  lacamentoService.criarLancamento(dto);
            return ResponseEntity.status(HttpStatus.CREATED).body(response);
 
 
+    }
+    @GetMapping("/relatorio")
+    public ResponseEntity<RelatorioResumoDto> gerarRelatorio(@RequestParam int mes,@RequestParam int ano){
+    RelatorioResumoDto relatorio = lacamentoService.gerarRelatorio(mes, ano);
+    return ResponseEntity.ok(relatorio);
     }
 }
