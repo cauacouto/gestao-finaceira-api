143b86b9ed55bc5e3a735c2bce1d735a422f4bfd
caua <cauacouto022@gmail.com>
2026-01-19 00:14:28 -0300

    feat: impletar atualizar, excluir

--- src/main/java/com/example/gestao/financeira/controller/LancamentoController.java
+++ src/main/java/com/example/gestao/financeira/controller/LancamentoController.java
@@ -1,5 +1,6 @@
 package com.example.gestao.financeira.controller;
 
+import com.example.gestao.financeira.DTO.AtualizaLancamento;
 import com.example.gestao.financeira.DTO.LacamentoRequestDto;
 import com.example.gestao.financeira.DTO.LacamentoResponseDto;
 import com.example.gestao.financeira.DTO.RelatorioResumoDto;
@@ -17,16 +18,27 @@ public class LancamentoController {
     public LancamentoController(LacamentoService lacamentoService) {
         this.lacamentoService = lacamentoService;
     }
-@PostMapping
+    @PostMapping
     public ResponseEntity<LacamentoResponseDto> criarLancamento(@RequestBody LacamentoRequestDto dto) {
-         LacamentoResponseDto response =  lacamentoService.criarLancamento(dto);
-           return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        LacamentoResponseDto response =  lacamentoService.criarLancamento(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
 
 
     }
     @GetMapping("/relatorio")
     public ResponseEntity<RelatorioResumoDto> gerarRelatorio(@RequestParam int mes,@RequestParam int ano){
-    RelatorioResumoDto relatorio = lacamentoService.gerarRelatorio(mes, ano);
-    return ResponseEntity.ok(relatorio);
+        RelatorioResumoDto relatorio = lacamentoService.gerarRelatorio(mes, ano);
+        return ResponseEntity.ok(relatorio);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<LacamentoResponseDto> atualizarParcialmente(@RequestBody @PathVariable Integer id, AtualizaLancamento dto){
+        LacamentoResponseDto response = lacamentoService.atualizarLancamento(id,dto);
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarLancamento(@PathVariable Integer id){
+        lacamentoService.excluirLancamento(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
